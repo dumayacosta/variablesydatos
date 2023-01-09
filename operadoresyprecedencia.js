d083f372172ffcb5fd93c813156cf418e01fbc92
dumayacosta <dumayacosta@gmail.com>
2023-01-08 23:34:28 -0400

    subiendo operaciones

---
+++ operadoresyprecedencia.js
@@ -0,0 +1,26 @@
+//Suma
+console.log(32 + 5);
+
+//Resta
+console.log(14 - 7);
+
+//Multiplicacion
+console.log(8 * 6);
+
+//Division
+console.log(30 / 9);
+
+//Resto
+console.log(95 % 4);
+
+//Prueba de precedencia
+console.log(3 + 5 * 2 - 8);
+
+//Prueba de precedencia 2
+console.log("HOLA" + " CAMILO");
+var lop1 = " CAMILO";
+var lop2 = "HOLA" + lop1;
+console.log(lop2);
+
+
+
