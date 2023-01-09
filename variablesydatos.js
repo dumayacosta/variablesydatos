56d6c1fef1d3428c87bf29d1b41de2d99f29e317
dumayacosta <dumayacosta@gmail.com>
2023-01-08 22:26:47 -0400

    Preparado para subir

---
+++ variablesydatos.js
@@ -0,0 +1,14 @@
+// variab strinng
+var amigo1 = "amigo1";
+// variable numeros
+var numeroentero = 17;
+var numdecimal = 12.2;
+var numnegativo = -2;
+// variable booleana
+var positovo = true;
+var negativa = false;
+// undefined o indefinida
+var amigo1;
+console.log(cajon1);
+// variable null, nula o vacia
+var nula = null;
